c89ac29cd4bb5ab75fb994dd6df19c80d777c269
yasharvindsingh <yashmaxnet@gmail.com>
2018-03-29 17:50:07 +0530

    committed

--- git.py
+++ git.py
@@ -4,7 +4,7 @@ import subprocess
 #subprocess.call('git init',shell = True)
 #print('init done..')
 
-#subprocess.call('git add .',shell=True)
+subprocess.call('git add .',shell=True)
 #print('adding dne')
 subprocess.call('git commit -m "committed"',shell=True)
 #subprocess.call('git remote add origin https://github.com/yasharvindsingh/oasystry.git',shell=True)
