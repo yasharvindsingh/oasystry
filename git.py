0d4313215a6026413c434194ecf25567e30d944e
yasharvindsingh <yashmaxnet@gmail.com>
2018-03-29 16:47:01 +0530

    committed

---
+++ git.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+#subprocess.call('git init',shell = True)
+#print('init done..')
+
+subprocess.call('git add .',shell=True)
+print('adding dne')
+subprocess.call('git commit -m "committed"',shell=True)
+subprocess.call('git remote add origin https://github.com/yasharvindsingh/oasystry.git',shell=True)
+subprocess.call('git push -f origin master',shell=True)
